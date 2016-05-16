598f90723c5fc97f9a2e8eab6939879a9adfb5bb
Steven <gFragatta@gmail.com>
2016-05-16 23:27:32 +0100

    Add company to assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SnapOnTop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany ("D-Flat Development")]
 [assembly: AssemblyProduct("SnapOnTop")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
